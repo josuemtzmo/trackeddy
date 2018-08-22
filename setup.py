e038afaab8e9ef4e5b36493dacfbd0109c052ad0
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2018-08-22 15:35:50 +1000

    [WIP] Documentation and tests  bug solved

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trackeddy',
-    version='b0.1',
+    version='0.1',
     description='Tracking of eddies in the ocean or any other gaussian like function in a 3D [f(t,x,y)] space.',
     url='https://github.com/Josue-Martinez-Moreno/eddy_identification',
     author='Josue Martinez Moreno',
@@ -10,8 +10,8 @@ setup(
     license='MIT License',
     packages=find_packages(),
     install_requires=['gsw','seawater'],
-    #packages=['trackeddy'],
-    #ext_package='trackeddy', 
+    packages=['trackeddy'],
+    ext_package='trackeddy', 
     zip_safe=False,
     test_suite='nose.collector',
     tests_require=['nose'],
