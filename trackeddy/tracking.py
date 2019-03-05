a8577fe48eb28e81f9ad5f794fac2356c71837c9
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2019-03-06 10:29:27 +1100

    Add np to function added to tracking

--- trackeddy/tracking.py
+++ trackeddy/tracking.py
@@ -544,7 +544,7 @@ def analyseddyzt(data,x,y,t0,t1,tstep,levels,areamap='',mask='',physics='',eddyc
         elif filters['spatial']['type'] == 'moving' and filters['spatial']['window'] != None:
             if filters['spatial']['mode'] == 'uniform':
                 nofilterdata = data[ii,:,:]
-                nofilterdata = nofilterdata - convolution.convolve(nofilterdata, kernel = ones((filters['spatial']['window'],filters['spatial']['window'])))
+                nofilterdata = nofilterdata - convolution.convolve(nofilterdata, kernel = np.ones((filters['spatial']['window'],filters['spatial']['window'])))
                 dataanomaly = ma.masked_array(nofilterdata, mask)
             if filters['spatial']['mode'] == 'gaussian':
                 raise Warning('ndimage.gaussian_filter may create artefacts near nan values. Therefore, data is filled with zeros.')
